41f5f5c68c38d3c4335eb8eb58032fd1cfeda2cd
Jose Bentancour <jose.bentancour@fing.edu.uy>
2018-04-23 15:00:14 -0300

    update button

--- button_gpio_expander_test.py
+++ button_gpio_expander_test.py
@@ -3,40 +3,47 @@
 from BDN_MCP23S17 import MCP23S17
 import rtmidi_python as rtmidi
 import time
+import sys
 
 MIDI_PORT_NAME = "FLUID"
 NOTE = 60
 VELOCITY = 127
+NOTE_ON = 0x90
+NOTE_OFF = 0x80
 
-midi_out = rtmidi.MidiOut()
+try:
+    midi_out = rtmidi.MidiOut()
+    port_found = False
+    for port_name in midi_out.ports:
+        if MIDI_PORT_NAME in port_name:
+            midi_out.open_port(port_name)
+            print "MIDI port foun!"
+            port_found = True
+    if not port_found:
+        print "MIDI port not found, exit."
+        sys.exit()
 
-for port_name in midi_out.ports:
-    if MIDI_PORT_NAME in port_name:
-        midi_out.open_port(port_name)
+    mcp1 = MCP23S17(ce=1)
+    mcp1.open()
 
-mcp1 = MCP23S17(ce=1)
-mcp1.open()
+    for x in range(0, 16):
+        mcp1.setDirection(x, mcp1.DIR_INPUT)
+        mcp1.setPullupMode(x, mcp1.PULLUP_ENABLED)
 
-for x in range(0, 16):
-    mcp1.setDirection(x, mcp1.DIR_INPUT)
-    mcp1.setPullupMode(x, mcp1.PULLUP_ENABLED)
+    old_btn = 1
 
-old_btn = 1
+    print "Starting reading pin A0 (CTRL+C to quit)"
+    while (True):
+        new_btn = mcp1.digitalRead(0)
+        if new_btn != old_btn:
+            if new_btn == 0:
+                #print "Button pressed!"
+                midi_out.send_message([NOTE_ON, NOTE, VELOCITY])
+            else:
+                #print "Button released!"
+                midi_out.send_message([NOTE_OFF, NOTE, VELOCITY])
 
-print "Starting reading pins (CTRL+C to quit)"
-while (True):
-    #data = mcp1.readGPIO()
-    #bin = "{0:b}".format(data) # binary
-    #print bin
-    #time.sleep(0.5)
+        old_btn = new_btn
 
-    new_btn = mcp1.digitalRead(0)
-    if new_btn != old_btn:
-        if new_btn == 0:
-            #print "Button pressed!"
-            midi_out.send_message([0x90, NOTE, VELOCITY])
-        else:
-            #print "Button released!"
-            midi_out.send_message([0x80, NOTE, VELOCITY])
-
-    old_btn = new_btn
+finally:
+    mcp1.close()
