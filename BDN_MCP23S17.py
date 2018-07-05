e113c21602be2f5507d0ece1f7b30ae1cba74911
Jose Bentancour <jose.bentancour@fing.edu.uy>
2018-07-05 19:26:26 -0300

    agrego funciones de PORT

--- BDN_MCP23S17.py
+++ BDN_MCP23S17.py
@@ -8,12 +8,10 @@ class MCP23S17(object):
     It is depndent on the Python packages spidev, which can
     be get from https://pypi.python.org/pypi/spidev.
     """
-    PULLUP_ENABLED = 0
-    PULLUP_DISABLED = 1
-
-    DIR_INPUT = 0
-    DIR_OUTPUT = 1
-
+    DIR_INPUT = 1
+    DIR_OUTPUT = 0
+    PULLUP_ENABLED = 1
+    PULLUP_DISABLED = 0
     LEVEL_LOW = 0
     LEVEL_HIGH = 1
 
@@ -22,8 +20,8 @@ class MCP23S17(object):
     """
     MCP23S17_IODIRA = 0x00
     MCP23S17_IODIRB = 0x01
-    MCP23S17_IPOLA = 0x2
-    MCP23S17_IPOLB = 0x3
+    MCP23S17_IPOLA = 0x02
+    MCP23S17_IPOLB = 0x03
     MCP23S17_GPIOA = 0x12
     MCP23S17_GPIOB = 0x13
     MCP23S17_OLATA = 0x14
@@ -57,41 +55,31 @@ class MCP23S17(object):
         ce -- The chip-enable number for the SPI
         deviceID -- The device ID of the component, i.e., the hardware address (default 0.0)
         """
-
         self.spi = spidev.SpiDev()
-
         self.bus = bus
         self.ce = ce
         self.deviceID = deviceID
-        self._GPIOA = 0
-        self._GPIOB = 0
-        self._IODIRA = 0
-        self._IODIRB = 0
-        self._GPPUA = 0
-        self._GPPUB = 0
-
+        self._GPIOA = 0x00
+        self._GPIOB = 0x00
+        self._IODIRA = 0xFF
+        self._IODIRB = 0xFF
+        self._GPPUA = 0x00
+        self._GPPUB = 0x00
         self.isInitialized = False
 
     def open(self):
         """Initializes the MCP23S17 with hardware-address access
         and sequential operations mode.
         """
-
         self.spi.open(self.bus, self.ce)
         self.spi.max_speed_hz = 10000000
         self.isInitialized = True
 
         self._writeRegister(MCP23S17.MCP23S17_IOCON, MCP23S17.IOCON_INIT)
 
-        # set defaults
-        for index in xrange(0, 15):
-            self.setDirection(index, MCP23S17.DIR_INPUT)
-            self.setPullupMode(index, MCP23S17.PULLUP_ENABLED)
-
     def close(self):
         """Closes the SPI connection that the MCP23S17 component is using.
         """
-
         self.spi.close()
         self.isInitialized = False
 
@@ -101,7 +89,6 @@ class MCP23S17(object):
         pin -- The pin index (0 - 15)
         mode -- The pull-up mode (MCP23S17.PULLUP_ENABLED, MCP23S17.PULLUP_DISABLED)
         """
-
         assert(pin < 16)
         assert((mode == MCP23S17.PULLUP_ENABLED)
                or (mode == MCP23S17.PULLUP_DISABLED))
@@ -134,7 +121,6 @@ class MCP23S17(object):
         pin -- The pin index (0 - 15)
         direction -- The direction of the pin (MCP23S17.DIR_INPUT, MCP23S17.DIR_OUTPUT)
         """
-
         assert (pin < 16)
         assert ((direction == MCP23S17.DIR_INPUT)
                 or (direction == MCP23S17.DIR_OUTPUT))
@@ -169,7 +155,6 @@ class MCP23S17(object):
          - MCP23S17.LEVEL_LOW, if the logical level of the pin is low,
          - MCP23S17.LEVEL_HIGH, otherwise.
         """
-
         assert(self.isInitialized)
         assert (pin < 16)
 
@@ -193,7 +178,6 @@ class MCP23S17(object):
         pin -- The pin idnex (0 - 15)
         level -- The logical level to be set (MCP23S17.LEVEL_LOW, MCP23S17.LEVEL_HIGH)
         """
-
         assert(self.isInitialized)
         assert(pin < 16)
         assert((level == MCP23S17.LEVEL_HIGH) or (level == MCP23S17.LEVEL_LOW))
@@ -219,12 +203,61 @@ class MCP23S17(object):
         else:
             self._GPIOB = data
 
+    def setDirPORTA(self, data):
+        assert(self.isInitialized)
+
+        self._writeRegister(MCP23S17.MCP23S17_IODIRA, data)
+        self._IODIRA = data
+
+    def setDirPORTB(self, data):
+        assert(self.isInitialized)
+
+        self._writeRegister(MCP23S17.MCP23S17_IODIRB, data)
+        self._IODIRA = data
+
+    def setPullupPORTA(self, data):
+        assert(self.isInitialized)
+
+        self._writeRegister(MCP23S17.MCP23S17_GPPUA, data)
+        self._GPPUA = data
+
+    def setPullupPORTB(self, data):
+        assert(self.isInitialized)
+
+        self._writeRegister(MCP23S17.MCP23S17_GPPUB, data)
+        self._GPPUB = data
+
+    def readPORTA(self):
+        assert(self.isInitialized)
+
+        data = self._readRegister(MCP23S17.MCP23S17_GPIOA)
+        self._GPIOA = data
+        return data
+
+    def readPORTB(self):
+        assert(self.isInitialized)
+
+        data = self._readRegister(MCP23S17.MCP23S17_GPIOB)
+        self._GPIOB = data
+        return data
+
+    def writePORTA(self,data):
+        assert(self.isInitialized)
+
+        self._writeRegister(MCP23S17.MCP23S17_GPIOA, data)
+        self._GPIOA = data
+
+    def writePORTB(self,data):
+        assert(self.isInitialized)
+
+        self._writeRegister(MCP23S17.MCP23S17_GPIOB, data)
+        self._GPIOB = data
+
     def writeGPIO(self, data):
         """Sets the data port value for all pins.
         Parameters:
         data - The 16-bit value to be set.
         """
-
         assert(self.isInitialized)
 
         self._GPIOA = (data & 0xFF)
@@ -236,7 +269,6 @@ class MCP23S17(object):
         Returns:
          - The 16-bit data port value
         """
-
         assert(self.isInitialized)
 
         data = self._readRegisterWord(MCP23S17.MCP23S17_GPIOA)
