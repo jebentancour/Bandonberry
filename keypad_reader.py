5463ff83ab084647e56cd2f2ca7785254a285c60
Jose Bentancour <jose.bentancour@fing.edu.uy>
2018-07-18 12:23:48 -0300

    mapeo completo

--- keypad_reader.py
+++ keypad_reader.py
@@ -18,6 +18,7 @@ current_dir = 1 # 0 = Abriendo, 1 = Cerrando
 
 right_notes_matrix = [[[0, 0] for x in range(ROWS)] for y in range(COLUMS)]
 # Mano Derecha
+# 0 = Abriendo, 1 = Cerrando
 right_notes_matrix[0][0] = [81, 80]
 right_notes_matrix[0][1] = [85, 88]
 right_notes_matrix[0][2] = [79, 75]
@@ -57,7 +58,45 @@ right_notes_matrix[5][1] = [91, 90]
 right_notes_matrix[5][2] = [89, 89]
 right_notes_matrix[5][3] = [94, 93]
 
-prev_data = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+left_notes_matrix = [[[0, 0] for x in range(ROWS)] for y in range(COLUMS)]
+# Mano Izquierda
+# 0 = Abriendo, 1 = Cerrando
+left_notes_matrix[0][0] = [ 54, 53]
+left_notes_matrix[0][1] = [ 39, 37]
+left_notes_matrix[0][2] = [ 36, 41]
+left_notes_matrix[0][3] = [ 63, 71]
+left_notes_matrix[0][4] = [ 69, 68]
+left_notes_matrix[0][5] = [ 67, 66]
+left_notes_matrix[0][6] = [ 47, 52]
+left_notes_matrix[0][7] = [ 38, 40]
+left_notes_matrix[1][0] = [ 66, 64]
+left_notes_matrix[1][1] = [ 61, 56]
+left_notes_matrix[1][2] = [ 42, 47]
+left_notes_matrix[1][3] = [ 62, 61]
+left_notes_matrix[1][4] = [ 59, 57]
+left_notes_matrix[1][5] = [ 56, 52]
+left_notes_matrix[1][6] = [ 52, 45]
+left_notes_matrix[2][0] = [ 48, 65]
+left_notes_matrix[2][1] = [ 43, 54]
+left_notes_matrix[2][2] = [ 41, 42]
+left_notes_matrix[2][3] = [ 64, 62]
+left_notes_matrix[2][4] = [ 60, 59]
+left_notes_matrix[2][5] = [ 57, 55]
+left_notes_matrix[2][6] = [ 50, 43]
+left_notes_matrix[3][0] = [ 51, 60]
+left_notes_matrix[3][1] = [ 65, 49]
+left_notes_matrix[3][2] = [ 58, 48]
+left_notes_matrix[3][3] = [ 55, 58]
+left_notes_matrix[3][4] = [ 45, 50]
+left_notes_matrix[3][5] = [ 40, 38]
+left_notes_matrix[4][0] = [ 49, 51]
+left_notes_matrix[4][1] = [ 53, 63]
+left_notes_matrix[4][2] = [ 68, 67]
+left_notes_matrix[4][3] = [ 46, 46]
+left_notes_matrix[4][4] = [ 44, 44]
+
+right_prev_data = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+left_prev_data = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
 
 try:
     midi_out = rtmidi.MidiOut()
@@ -73,34 +112,60 @@ try:
         print "MIDI port not found, exit."
         sys.exit()
 
+    # Izquierda
     mcp1 = MCP23S17(ce=1)
     mcp1.open()
     mcp1.setDirPORTA(0xFF)
     mcp1.setDirPORTB(0xC0)
     mcp1.setPullupPORTA(0xFF)
     mcp1.setPullupPORTB(0xC0)
+    # Derecha
+    mcp0 = MCP23S17(ce=0)
+    mcp0.open()
+    mcp0.setDirPORTA(0xFF)
+    mcp0.setDirPORTB(0xC0)
+    mcp0.setPullupPORTA(0xFF)
+    mcp0.setPullupPORTB(0xC0)
 
     while (True):
-        new_data = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        left_new_data = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        right_new_data = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
         for current_col in range(COLUMS):
             mcp1.writePORTB(~(0x01 << current_col))
+            mcp0.writePORTB(~(0x01 << current_col))
             for i in range(NUM_OF_READS):
-                new_data[current_col] |= mcp1.readPORTA()
+                left_new_data[current_col] |= mcp1.readPORTA()
+                right_new_data[current_col] |= mcp0.readPORTA()
                 time.sleep(DEBOUNCE_DELAY)
-            if prev_data[current_col] != new_data[current_col]:
+            if left_prev_data[current_col] != left_new_data[current_col]:
                 for current_row in range(ROWS):
-                    new_bit = (new_data[current_col] >> current_row) & 0x01
-                    prev_bit = (prev_data[current_col] >> current_row) & 0x01
+                    new_bit = (left_new_data[current_col] >> current_row) & 0x01
+                    prev_bit = (left_prev_data[current_col] >> current_row) & 0x01
+                    if new_bit != prev_bit:
+                        if new_bit:
+                            note = left_notes_matrix[current_col][current_row][current_dir]
+                            midi_out.send_message([NOTE_OFF, note, VELOCITY])
+                            print 'left_OFF [{0}][{1}] {2}'.format(current_col, current_row, note)
+                        else:
+                            note = left_notes_matrix[current_col][current_row][current_dir]
+                            midi_out.send_message([NOTE_ON, note, VELOCITY])
+                            print 'left_ON [{0}][{1}] {2}'.format(current_col, current_row, note)
+            if right_prev_data[current_col] != right_new_data[current_col]:
+                for current_row in range(ROWS):
+                    new_bit = (right_new_data[current_col] >> current_row) & 0x01
+                    prev_bit = (right_prev_data[current_col] >> current_row) & 0x01
                     if new_bit != prev_bit:
                         if new_bit:
                             note = right_notes_matrix[current_col][current_row][current_dir]
                             midi_out.send_message([NOTE_OFF, note, VELOCITY])
-                            print 'Note OFF [{0}][{1}] {2}'.format(current_col, current_row, note)
+                            print 'right_OFF [{0}][{1}] {2}'.format(current_col, current_row, note)
                         else:
                             note = right_notes_matrix[current_col][current_row][current_dir]
                             midi_out.send_message([NOTE_ON, note, VELOCITY])
-                            print 'Note ON [{0}][{1}] {2}'.format(current_col, current_row, note)
-        prev_data = new_data
+                            print 'right_ON [{0}][{1}] {2}'.format(current_col, current_row, note)
+        left_prev_data = left_new_data
+        right_prev_data = right_new_data
 
 finally:
     mcp1.close()
+    mcp0.close()
