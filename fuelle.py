40efedb9d94d716f139d12df9469a8d12fdeae09
jebentancour <jose.bentancour@fing.edu.uy>
2019-03-11 16:54:56 -0300

    fuelle stereo

--- fuelle.py
+++ fuelle.py
@@ -6,7 +6,7 @@ import Adafruit_BMP.BMP085 as BMP085
 import rtmidi_python as rtmidi
 import time
 
-## Varias opciones
+## Varias opciones de muestreo
 sensor = BMP085.BMP085(mode=BMP085.BMP085_STANDARD)
 ## sensor = BMP085.BMP085(mode=BMP085.BMP085_ULTRALOWPOWER)
 ## sensor = BMP085.BMP085(mode=BMP085.BMP085_HIGHRES)
@@ -14,12 +14,10 @@ sensor = BMP085.BMP085(mode=BMP085.BMP085_STANDARD)
 
 ## Etiquetas MIDI
 CONTROL = 0xB0
-CUSTOM = 0x09
 VOLUME = 0x07
 
 ## Puertos MIDI
 SYNTH_PORT_NAME = "FLUID"
-KEYPAD_PORT_NAME = "KEYPAD"
 
 ## Presión atmosférica
 presion = 0
@@ -38,23 +36,13 @@ tope = 127
 min = 0
 
 # Me conecto con el puerto MIDI del sintetizador
-midi_out1 = rtmidi.MidiOut()
+midi_out = rtmidi.MidiOut()
 port_found = False
 while not port_found:
-    for port_name in midi_out1.ports:
+    for port_name in midi_out.ports:
         if SYNTH_PORT_NAME in port_name:
-            midi_out1.open_port(port_name)
+            midi_out.open_port(port_name)
             print "Puerto sintetizador encontrado"
-            port_found = True
-
-# Me conecto con el puerto MIDI del teclado
-midi_out2 = rtmidi.MidiOut()
-port_found = False
-while not port_found:
-    for port_name in midi_out2.ports:
-        if KEYPAD_PORT_NAME in port_name:
-            midi_out2.open_port(port_name)
-            print "Puerto KeyPad encontrado"
             port_found = True
 
 # Comienzan las lecturas del sensor
@@ -63,27 +51,39 @@ if (presion - media) > 0:
     estado = 1 ## Cerrando
 else:
     estado = 0 ## Abriendo
+
 while True:
+    # Mido
     presion = (presion + sensor.read_pressure())/2
-    print('Presion = {0:0.2f} Pa'.format(presion))
-    if (presion - media) > 0 and estado == 0:
-        print "Cerrando"
-        estado = 1
-        midi_out2.send_message([CONTROL, CUSTOM, 0x01]) # Fuelle cerrando
-    elif (presion -media) < 0 and estado == 1:
-        print "Abriendo"
-        estado = 0
-        midi_out2.send_message([CONTROL, CUSTOM, 0x00]) # Fuelle abriendo
-    else:
-        pass
+    # print('Presion = {0:0.2f} Pa'.format(presion))
+
+    # Calculo
     value = abs(presion-media)/sensibilidad
-    print('Volume = {0:0.2f}'.format(((abs(presion-media))/sensibilidad)))
+    # print('Volume = {0:0.2f}'.format(((abs(presion-media))/sensibilidad)))
+
+    # Checkeo extremos
     if value < min:
-        midi_out1.send_message([CONTROL, VOLUME, 0])
+        value = 0;
+    if value > tope:
+        value = tope;
+
+    if (presion - media) > 0:
+        # Cerrando
+        midi_out.send_message([CONTROL | 0x00, VOLUME, 0x00]) # Mano derecha abriendo
+        midi_out.send_message([CONTROL | 0x01, VOLUME, value]) # Mano derecha cerrando
+        midi_out.send_message([CONTROL | 0x02, VOLUME, 0x00]) # Mano izquierda abriendo
+        midi_out.send_message([CONTROL | 0x03, VOLUME, value]) # Mano izquierda cerrando
+    elif (presion - media) < 0:
+        # Abriendo
+        midi_out.send_message([CONTROL | 0x00, VOLUME, value]) # Mano derecha abriendo
+        midi_out.send_message([CONTROL | 0x01, VOLUME, 0x00]) # Mano derecha cerrando
+        midi_out.send_message([CONTROL | 0x02, VOLUME, value]) # Mano izquierda abriendo
+        midi_out.send_message([CONTROL | 0x03, VOLUME, 0x00]) # Mano izquierda cerrando
     else:
-        if (value) < 127:
-            midi_out1.send_message([CONTROL, VOLUME, value])
-        else:
-            midi_out1.send_message([CONTROL, VOLUME, tope])
-    #time.sleep(0.2)
+        midi_out.send_message([CONTROL | 0x00, VOLUME, 0x00]) # Mano derecha abriendo
+        midi_out.send_message([CONTROL | 0x01, VOLUME, 0x00]) # Mano derecha cerrando
+        midi_out.send_message([CONTROL | 0x02, VOLUME, 0x00]) # Mano izquierda abriendo
+        midi_out.send_message([CONTROL | 0x03, VOLUME, 0x00]) # Mano izquierda cerrando
+
+    # Calculo la media
     media = media * 0.999 + presion * 0.001
