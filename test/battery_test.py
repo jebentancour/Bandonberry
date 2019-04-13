478e7d5028c55a671cfbd005f46d51e5a87c42a6
Jose Bentancour <josebentancour@gmail.com>
2019-04-13 09:41:22 -0300

    Update battery_test.py

--- test/battery_test.py
+++ test/battery_test.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
-
+import time
 import rtmidi_python as rtmidi
 import RPi.GPIO as GPIO
-import time
+
 
 NOTE_ON = 0x90
 NOTE_OFF = 0x80
 
 MIDI_PORT_NAME = "FLUID"
-VELOCITY = 127
-NOTE = 60
+VELOCITY = 64
+NOTE1 = 60
+NOTE2 = 68 
 
 midi_out = rtmidi.MidiOut()
 for port_name in midi_out.ports:
@@ -22,21 +23,18 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN, GPIO.OUT)
 
 servo = GPIO.PWM(PIN,50)
-servo.start(7.5)                                          # generate PWM signal with 7.5% duty cycle
+servo.start(7.5)                                   # generate PWM signal with 7.5% duty cycle
 
-try:
-    while True:                                           # execute loop forever
-        servo.ChangeDutyCycle(5)
-        time.sleep(0.5)                                     # sleep for 1 second
-        servo.ChangeDutyCycle(0)
-        time.sleep(0.5)                                     # sleep for 1 second
-        servo.ChangeDutyCycle(10)
-        time.sleep(0.5)                                     # sleep for 1 second
-        servo.ChangeDutyCycle(0)
-        midi_out.send_message([NOTE_ON, NOTE, VELOCITY])
-        time.sleep(2)                                       # sleep for 1 second
-        midi_out.send_message([NOTE_OFF, NOTE, VELOCITY])
+midi_out.send_message([NOTE_ON, NOTE1, VELOCITY])  # Toco notas
+midi_out.send_message([NOTE_ON, NOTE2, VELOCITY])
+
+# Muevo el motor en un loop infinito
+while True:                                           # execute loop forever
+        servo.ChangeDutyCycle(8)                      # Servo abierto
+        time.sleep(0.5)                                   
+        servo.ChangeDutyCycle(0)                      # Servo quieto
         time.sleep(0.5)
-except KeyboardInterrupt:
-    servo.stop()
-    GPIO.cleanup()
+        servo.ChangeDutyCycle(5)                      # Cerrado
+        time.sleep(0.5)
+        servo.ChangeDutyCycle(0)
+        time.sleep(0.5)
